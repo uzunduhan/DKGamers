7c43309fe0d9eadde919bc5c40cd77437541dac2
duhan_uzun <51044577+duhan18@users.noreply.github.com>
2020-12-25 18:43:21 +0300

    sayfalar eklendi ve tasarlandı, genel şema çıkartıldı

---
+++ DKGamers/Controllers/AksiyonController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DKGamers.Controllers
+{
+    public class AksiyonController : Controller
+    {
+
+        public IActionResult Cok()
+        {
+            return View();
+        }
+
+        public IActionResult Tek()
+        {
+            return View();
+        }
+
+       
+    }
+}
